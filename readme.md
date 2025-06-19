0d4ecff50f224bdf4f88f2dfdb3295e627e268d0
Mitchell Tishmack <git@mitchty.net>
2025-06-19 09:55:59 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Kairos
+
+When I get more time I'll expand this but for now don't use this thing unless you have the same simple use case I implemented so far.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use chrono::{Datelike, Local, Timelike};
+
 fn main() {
-    println!("Hello, world!");
+    let now = Local::now();
+    let is_weekday = now.weekday().number_from_monday() <= 5;
+    let hour = now.hour();
+    let in_worktime = hour >= 8 && hour < 18;
+
+    if is_weekday && in_worktime {
+        std::process::exit(1);
+    } else {
+        std::process::exit(0);
+    }
 }
